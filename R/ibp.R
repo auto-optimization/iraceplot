8a995db06394f86249660c879c62efb3198a1cd0
pabloOnate <31405588+pabloOnate@users.noreply.github.com>
2021-01-05 09:44:54 -0300

    Se modifican los nombres de los ejes del grafico

--- R/ibp.R
+++ R/ibp.R
@@ -10,7 +10,7 @@
 #' @return plot
 #'
 #' @importFrom stats reshape
-#' @importFrom ggplot2 ggplot geom_boxplot geom_jitter position_jitter aes
+#' @importFrom ggplot2 ggplot geom_boxplot geom_jitter position_jitter aes theme
 #'
 #' @export
 #'
@@ -18,12 +18,12 @@
 #' NULL
 
 ibp <- function(iraceResults){
-  ID <- Datos <- NULL
+  Performance <- Elite_configuration <- NULL
   long <- length(iraceResults$allElites)
   id <- iraceResults$allElites[[long]]
   matriz <- as.data.frame(iraceResults$experiments[,id])
   n_row_col = as.numeric(dim(matriz)[1]*dim(matriz)[2])
-  tabla <- reshape(matriz,varying = c(as.character(id)), v.names = "Datos", timevar = "ID",times = c(as.character(id)),new.row.names = 1:n_row_col,direction = "long")
-  p <- ggplot(tabla, aes(x=ID,y=Datos,color=ID)) + geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2))
+  tabla <- reshape(matriz,varying = c(as.character(id)), v.names = "Performance", timevar = "Elite_configuration",times = c(as.character(id)),new.row.names = 1:n_row_col,direction = "long")
+  p <- ggplot(tabla, aes(x=Elite_configuration,y=Performance,color=Elite_configuration)) + geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2)) + theme(legend.position="none")
   p
 }
