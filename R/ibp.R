fc92734a0ae7fe87cdbe57e61dd8d2e64c667033
pabloOnate <31405588+pabloOnate@users.noreply.github.com>
2021-01-05 13:10:05 -0300

    Se agrega el parametro numerIteration a la funcion ibp

--- R/ibp.R
+++ R/ibp.R
@@ -1,7 +1,7 @@
 #' Graphic box plot
 #'
-#'Create a graphic of box plot from the latest iteration
-#'of irace using the best quality id
+#'Create a graphic of box plot of irace using the best quality id
+#'By default the graph of the last iteration is shown
 #'
 #' @param iraceResults
 #'
@@ -9,10 +9,13 @@
 #'
 #' @param fileName
 #'
-#'It is of type string
+#'It is of type string.
 #'You must place the path where you want to save the file and its name, it is not necessary to place the extension of the pdf
-#'If you do not put the route it will be saved in the current location where you are working
-#'If you do not want to save the file and the graph is displayed on the screen, just put NULL in the argument
+#'
+#' @param numberIteration
+#'
+#' It's of type numeric.
+#' Allows to choose which iteration of the elite configuration of irace to graph
 #'
 #' @return plot
 #'
@@ -25,11 +28,21 @@
 #' @examples
 #' NULL
 
-ibp <- function(iraceResults, fileName){
+ibp <- function(iraceResults, fileName = NULL,numberIteration = NULL){
   Performance <- Elite_configuration <- NULL
   long <- length(iraceResults$allElites)
+  if(!is.null(numberIteration)){
+    if(numberIteration <= long){
+      long <- numberIteration
+    }else{
+      return(print("iteration number out of range"))
+    }
+  }
   id <- iraceResults$allElites[[long]]
   matriz <- as.data.frame(iraceResults$experiments[,id])
+  if(length(id) == 1){
+    colnames(matriz)[colnames(matriz) == "iraceResults$experiments[, id]"] <- id
+  }
   n_row_col = as.numeric(dim(matriz)[1]*dim(matriz)[2])
   tabla <- reshape(matriz,varying = c(as.character(id)), v.names = "Performance", timevar = "Elite_configuration",times = c(as.character(id)),new.row.names = 1:n_row_col,direction = "long")
   p <- ggplot(tabla, aes(x=Elite_configuration,y=Performance,color=Elite_configuration)) + geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2)) + theme(legend.position="none")
