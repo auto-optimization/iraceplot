b85be8650e4f7e8d65e547a52a80639c6ba21de6
pabloOnate <31405588+pabloOnate@users.noreply.github.com>
2021-01-20 14:16:27 -0300

    Se crea el parcoord() de forma interactiva con shiny app

--- R/iparcoord.R
+++ R/iparcoord.R
@@ -6,25 +6,41 @@
 #' @param idIteration
 #'It is a vector with id values that you want to graph
 #'
-#' @param fileName
+#' @param param_names
 #'
-#' It's of type string
-#'You must place the path where you want to save the file and its name without the extension pdf
+#' It's of type vector
+#' is a vector with the parameters to be displayed in the plot
 #'
 #' @return plot
 #' @export
 #'
 #'@importFrom dplyr arrange
 #'@importFrom parcoords parcoords
+#'@importFrom shinydashboard  dashboardBody dashboardPage dashboardHeader dashboardSidebar box
+#'@importFrom shiny dataTableOutput renderDataTable shinyApp fluidRow checkboxGroupInput h3
 #'
 #' @examples
 #' NULL
 
-iparcoord <- function(iraceResults, idIteration = NULL,fileName = NULL){
+iparcoord <- function(iraceResults, idIteration = NULL, param_names = NULL){
 
   #Variable assignment
-  memo  <- configuration <- NULL
+  memo  <- configuration <- dim <- choi <- NULL
   idIteration <- unlist(idIteration)
+  param_names <- unlist(param_names)
+
+  #verify that param_names is other than null
+  if(!is.null(param_names)){
+    #verify that param_names contain the data entered
+    if( "FALSE" %in% names(table(param_names %in% iraceResults$parameters$names))){
+      return("Some wrong parameter entered")
+    }
+    #verify that param_names contain more than one parameter
+    else if(length(param_names) < 2){
+      return("You must enter at least two parameters")
+    }
+
+  }
 
   if(!is.null(idIteration)){
 
@@ -85,34 +101,133 @@ iparcoord <- function(iraceResults, idIteration = NULL,fileName = NULL){
 
   # Column .ID. and .PARENT. are removed
   tabla <- tabla[, !(names(tabla) %in% c(".ID.",".PARENT."))]
+  if(!is.null(param_names)){
+    param_names <- c(param_names,"iteration")
+    tabla <- tabla[, (names(tabla) %in% param_names )]
+  }
 
   # NA are converted to character in the table
   for(k in 1:(length(tabla))){
     tabla[[k]][is.na(tabla[[k]])] <- "NA"
   }
 
-  # The graph is created
-  p<-parcoords::parcoords(tabla,
-    rownames = FALSE,
-    reorder=TRUE,
-    brushMode = "1D",
-    color = list(
-    colorBy = "iteration",
-    colorScale = "scaleOrdinal",
-    colorScheme = "schemeCategory10"),
-    withD3 = TRUE
-    )
-
-  #If the value in fileName is added the pdf file is created
-  if(!is.null(fileName)){
-    #The fileName value is worked to separate it and assign it to new values.
-    nameFile = basename(fileName)
-    directory = paste0(dirname(fileName),sep="/")
-    withr::with_dir(directory, orca(p, paste0(nameFile,".pdf")))
-
-  #If you do not add the value of fileName, the plot is displayed
-  }else{
-    p
+  #The dimension is modified for certain parameters in the plot
+  if("rasrank" %in% colnames(tabla) && "elitistants" %in% colnames(tabla)){
+    dim = list(
+      rasrank = list(
+        tickValues = c(min(tabla$rasrank),
+                     iraceResults$parameters$domain[["rasrank"]][2]/4,
+                     iraceResults$parameters$domain[["rasrank"]][2]/2,
+                     max(tabla$rasrank))
+      ),
+      elitistants = list(
+        tickValues = c(min(tabla$elitistants),
+                     450,
+                     max(tabla$elitistants))
+        )
+      )
   }
+
+  #A list is created based on the iterations of the table
+  for(m in min(tabla$iteration):max(tabla$iteration)){
+    texto = paste("Iteration",m)
+    choi[[texto]] <- m
+  }
+
+
+  #shiny app body
+  body <- dashboardBody(
+    fluidRow(
+      # parallel coordinate plot box
+      box(
+        title = "Irace Parcoords Plot", width = 10, status = "primary",
+        parcoords::parcoordsOutput("tablaPlot")
+      ),
+      # iterations box
+      box(
+        width = 2,
+        checkboxGroupInput("checkGroup",h3("Iterations"),
+                           choices =  choi,
+                           selected = c(min(tabla$iteration):max(tabla$iteration)))
+      )
+    ),
+    # Data box
+    fluidRow(
+      box(
+        title = "Extracted Data", status = "primary",
+        dataTableOutput("SelectedData")
+      )
+    )
+  )
+
+  ui <- dashboardPage(
+    dashboardHeader(title="Parcoords Plot"),
+    dashboardSidebar(disable=TRUE),
+    body
+  )
+
+  server = function(input, output, session) {
+
+
+    #Standard parcoords plot in shiny
+
+    output$tablaPlot <- parcoords::renderParcoords({
+      tablaOut <- tabla[(tabla$iteration %in% input$checkGroup),]
+      parcoords::parcoords(tablaOut,
+                           rownames = FALSE,
+                           reorder=TRUE,
+                           brushMode = "1D",
+                           color = list(
+                             colorBy = "iteration",
+                             colorScale = "scaleOrdinal",
+                             colorScheme = "schemePaired"),
+                           withD3 = TRUE,
+                           autoresize = TRUE,
+                           dimensions = dim,
+                           tasks = list(htmlwidgets::JS(
+                             "
+    function(){
+      // supply an array of columns to hide
+      this.parcoords.hideAxis(['names','iteration'])
+
+      this.parcoords.removeAxes();
+      this.parcoords.render();
+
+      // duplicated from the widget js code
+      //  to make sure reorderable and brushes work
+      if( this.x.options.reorderable ) {
+        this.parcoords.reorderable();
+      } else {
+        this.parcoords.createAxes();
+      }
+
+      if( this.x.options.brushMode ) {
+      // reset the brush with None
+        this.parcoords.brushMode('None')
+        this.parcoords.brushMode(this.x.options.brushMode);
+        this.parcoords.brushPredicate(this.x.options.brushPredicate);
+      }
+
+      // delete title from the rownames axis
+      //d3.select('#' + this.el.id + ' .dimension .axis > text').remove();
+    }
+    "
+                           ))
+      )
+
+    })
+
+
+    # Here we can access the variable input$id_rows to determine which are selected
+    # we display these results in a table
+    output$SelectedData <- renderDataTable({
+      ids <- rownames(tabla) %in% input$tablaPlot_brushed_row_names
+      tabla[ids,]
+    })
+
+  }
+
+  shinyApp(ui, server)
+
 }
 
