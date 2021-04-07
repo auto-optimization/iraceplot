a28ab4b286d66ca8f2cfb34acfc9135fe8029c5f
pabloOnate <31405588+pabloOnate@users.noreply.github.com>
2021-04-07 02:58:55 -0400

    El report se crea en pdf, se puede indicar el archivo de entrada asi como la ubicacion de guardado

--- R/report.R
+++ R/report.R
@@ -1,5 +1,13 @@
 #' Report Iraceplot
 #'
+#' @param iraceResults
+#' irace log variable obtained from the Rdata file generate by irace
+#'
+#' @param fileName
+#' String, file name indicating where to save a pdf file with the plot.
+#' A pdf extension will be added to the file name provided
+#' (example: "~/path-to/filename")
+#'
 #' @return document
 #' @export
 #'
@@ -13,7 +21,7 @@ report <- function(iraceResults, fileName = NULL){
   filename <- NULL
 
   reportes <- tibble(
-    filename = stringr::str_c(paste0(fileName,".docx"))
+    filename = stringr::str_c(paste0(fileName,".pdf"))
   )
 
   reportes %>%
