29d4256e359e1e2c8a217b76e5095b3857a53f46
Leslie Perez Caceres <leslie@Yendegaia.local>
2021-08-11 20:34:51 -0300

    report changes

--- R/report.R
+++ R/report.R
@@ -2,6 +2,9 @@
 #' 
 #' The `report` function creates an HTML report of the most relevant irace log
 #' data.
+#' 
+#' This report provides general statistics and plots that show the best configurations
+#' and their performance.
 #'
 #' @template arg_irace_results
 #'
@@ -21,16 +24,15 @@ report <- function(irace_results,  file_name) {
     stop()
   } 
   
-
   final_file <- path_rel2abs(paste0(file_name, ".html"))
   # Output file name is generated
   reportes <- tibble(
-    file_name = stringr::str_c(paste0(file_name, ".html"))
+    filename = stringr::str_c(paste0(file_name, ".html"))
   )
   cat("Creating file", final_file, "\n")
   # The output location of the file is placed as well as the location report_html.Rmd
   reportes %>%
-    select(output_file = file_name) %>%
+    select(output_file = filename) %>%
     purrr::pwalk(rmarkdown::render, input = system.file("template", "report_html.Rmd", package = "iraceplot"))
   
   browseURL(final_file)
