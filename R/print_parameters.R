180280ef5cf93572b763e09ffef4df8f3f1caef5
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2022-06-07 00:27:14 +0200

    * R/print_parameters.R: Add option digits

--- R/print_parameters.R
+++ R/print_parameters.R
@@ -2,11 +2,13 @@
 #'
 #' @param params (`list()`) Parameter object stored in `irace.Rdata` or read with `irace::readParameters()`.
 #'
+#' @param digits (`integer()`) The desired number of digits after the decimal point for real-valued parameters. Default is 15, but it should be the value in `scenario$digits`.
+#' 
 #' @examples
 #' load(system.file(package="iraceplot", "exdata", "guide-example.Rdata", mustWork = TRUE))
 #' printParameters(iraceResults$parameters)
 #' @export
-printParameters <- function(params)
+printParameters <- function(params, digits = 15L)
 {
   names_len <- max(nchar(params$names))
   switches_len <- max(nchar(params$switches)) + 2
@@ -15,10 +17,10 @@ printParameters <- function(params)
     type <- params$types[[name]]
     domain <- params$domain[[name]]
     # FIXME: We should use digits
-    if (type == "r") domain <- sprintf("%.17g,%.17g", domain[1], domain[2])
+    if (type == "r") domain <- formatC(domain, digits=digits, format="f", drop0trailing=TRUE)
     domain <- paste0('(', paste0(domain, collapse=","), ')')
     condition <- params$conditions[[name]]
     condition <- if (isTRUE(condition)) "" else paste0(" | ", condition)
-    cat(sprintf('%*s %*s %s %15s%s\n', names_len, name, switches_len, switch, type, domain, condition))
+    cat(sprintf('%*s %*s %s %-15s%s\n', -names_len, name, -switches_len, switch, type, domain, condition))
   }
 }
