01c16c83cfeaa52b74c3123b8222868d6133d23f
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2022-06-30 16:50:34 +0200

    * R/print_parameters.R: handle transforms.

--- R/print_parameters.R
+++ R/print_parameters.R
@@ -1,12 +1,23 @@
 #' Print parameter space in the textual format accepted by irace.
+#' 
+#' FIXME: Dependent parameter bounds are not supported yet. This function will move to the irace package in the next release.
 #'
 #' @param params (`list()`) Parameter object stored in `irace.Rdata` or read with `irace::readParameters()`.
 #'
 #' @param digits (`integer()`) The desired number of digits after the decimal point for real-valued parameters. Default is 15, but it should be the value in `scenario$digits`.
 #' 
 #' @examples
-#' load(system.file(package="iraceplot", "exdata", "guide-example.Rdata", mustWork = TRUE))
-#' printParameters(iraceResults$parameters)
+#' @examples
+#'  parameters.table <- '
+#'  # name       switch           type  values               [conditions (using R syntax)]
+#'  algorithm    "--"             c     (as,mmas,eas,ras,acs)
+#'  localsearch  "--localsearch " c     (0, 1, 2, 3)
+#'  ants         "--ants "        i,log (5, 100)
+#'  q0           "--q0 "          r     (0.0, 1.0)           | algorithm == "acs"
+#'  nnls         "--nnls "        i     (5, 50)              | localsearch %in% c(1,2,3)
+#'  '
+#' parameters <- readParameters(text=parameters.table)
+#' printParameters(parameters)
 #' @export
 printParameters <- function(params, digits = 15L)
 {
@@ -15,12 +26,13 @@ printParameters <- function(params, digits = 15L)
   for (name in params$names) {
     switch <- paste0('"', params$switches[[name]], '"')
     type <- params$types[[name]]
+    transf <- params$transform[[name]]
     domain <- params$domain[[name]]
-    # FIXME: We should use digits
     if (type == "r") domain <- formatC(domain, digits=digits, format="f", drop0trailing=TRUE)
     domain <- paste0('(', paste0(domain, collapse=","), ')')
     condition <- params$conditions[[name]]
     condition <- if (isTRUE(condition)) "" else paste0(" | ", condition)
+    if (!is.null(transf) && transf != "") type <- paste0(type, ",", transf)
     cat(sprintf('%*s %*s %s %-15s%s\n', -names_len, name, -switches_len, switch, type, domain, condition))
   }
 }
